01c98ffb21e5a314994be3e9cb68d4553d580ef2
holterhus <rileyholterhus@gmail.com>
2020-11-22 03:32:49 -0500

    Updates

---
+++ precache-manifest.58e3117e58d222ebb40bf4ca832ef4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa15f9705a16e11c38099bfa83f24c0f",
+    "url": "/pokemonCNN/index.html"
+  },
+  {
+    "revision": "fd0be097ed2e8ba62d62",
+    "url": "/pokemonCNN/static/css/main.2da41573.chunk.css"
+  },
+  {
+    "revision": "e5d7ba48245565ede0b4",
+    "url": "/pokemonCNN/static/js/2.14fe2f59.chunk.js"
+  },
+  {
+    "revision": "a244fffae27d60e5a79fcf3e76cfa315",
+    "url": "/pokemonCNN/static/js/2.14fe2f59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd0be097ed2e8ba62d62",
+    "url": "/pokemonCNN/static/js/main.95103e25.chunk.js"
+  },
+  {
+    "revision": "ae162e40ac8bab5e7b57",
+    "url": "/pokemonCNN/static/js/runtime-main.5d743ae5.js"
+  }
+]);
